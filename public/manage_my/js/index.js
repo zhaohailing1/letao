d61bd24497395d00d9b1796008239aaac5fdb366
“aufumnfish <291330512@qq.com>
2017-12-08 15:52:13 +0800

    完成首页基本功能 图表展示

---
+++ public/manage_my/js/index.js
@@ -0,0 +1,71 @@
+  $(function(){
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById('main_left'));
+
+      // 指定图表的配置项和数据
+      var option = {
+          title: {
+              text: '2017注册人数'
+          },
+          tooltip: {},
+          legend: {
+              data: ['人数']
+          },
+          xAxis: {
+              data: ["1月", "2月", "3月", "4月", "5月", "6月"]
+          },
+          yAxis: {},
+          series: [{
+              name: '人数',
+              type: 'bar',
+              data: [5, 20, 36, 15, 10, 20]
+          }]
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+
+
+      // 基于准备好的dom，初始化echarts实例
+      var myChart2 = echarts.init(document.getElementById('main_right'));
+      option2 = {
+          title: {
+              text: '某站点用户访问来源',
+              subtext: '纯属虚构',
+              x: 'center'
+          },
+          tooltip: {
+              trigger: 'item',
+              formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+          },
+          series: [
+              {
+                  name: '访问来源',
+                  type: 'pie',
+                  radius: '55%',
+                  center: ['50%', '60%'],
+                  data: [
+                      { value: 335, name: '直接访问' },
+                      { value: 310, name: '邮件营销' },
+                      { value: 234, name: '联盟广告' },
+                      { value: 135, name: '视频广告' },
+                      { value: 1548, name: '搜索引擎' }
+                  ],
+                  itemStyle: {
+                      emphasis: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      };
+
+      myChart2.setOption(option2);
+  })
